91ccc81fc7826da5548482bd57c843eafa4f3bc6
pratg35 <117987440+pratg35@users.noreply.github.com>
2024-03-23 11:48:56 +0000

    deleted files

--- sentiment-api/test.py
+++
@@ -1,4 +0,0 @@
-from transformers import pipeline
-sentiment_pipeline = pipeline("sentiment-analysis")
-data = ["It was the best of times.", "t was the worst of times."]
-print(sentiment_pipeline(data))
